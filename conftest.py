f095da91461cfc290788982d06f70604afbeded0
Svetlana <levinlights@gmail.com>
2026-04-08 15:39:19 +0700

    fix_conftest

--- conftest.py
+++ conftest.py
@@ -4,19 +4,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# Для открытия и закрытия тестов в Chrome
-@pytest.fixture(scope="function")
-def browser():
-    driver = webdriver.Chrome()
+# Для открытия и закрытия тестов в Chrome и в Firefox
+@pytest.fixture(scope="function", params=["chrome", "firefox"])
+def browser(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    else:
+        driver = webdriver.Firefox()
     yield driver
     driver.quit()
-
-# Для открытия и закрытия тестов в Chrome и в Firefox
-# @pytest.fixture(scope="function", params=["chrome", "firefox"])
-# def browser(request):
-    # if request.param == "chrome":
-    #     driver = webdriver.Chrome()
-    # else:
-    #     driver = webdriver.Firefox()
-    # yield driver
-    # driver.quit()
