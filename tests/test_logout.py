d957df9af839bc2240275fad85f2106bb6dd622e
Svetlana <levinlights@gmail.com>
2026-04-09 15:47:14 +0700

    update test_logout, add user registration

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -5,23 +5,27 @@ from pages.locators import HEADERS_LOCATORS, LOGIN_PAGE_LOCATORS, PROFILE_PAGE_L
 
 from data.config import LOGIN_URL
 
-from utils import login_user
+from utils import login_user, register_user
 
 
 class TestLogout:
     # Тест на выход из аккаунта по кнопке «Выйти» в личном кабинете
     def test_successful_logout(self, browser):
+        email, password = register_user(browser)  # Регистрация пользователя
+
         browser.get(LOGIN_URL)
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((LOGIN_PAGE_LOCATORS["h2_enter"])))
-        login_user(browser)  # Авторизация пользователя
 
+        login_user(browser, email, password)  # Авторизация пользователя
         browser.find_element(*HEADERS_LOCATORS["account_link"]).click()
-
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((PROFILE_PAGE_LOCATORS["save_button"])))
 
-        browser.find_element(*PROFILE_PAGE_LOCATORS["exit_button"]).click()
+        exit_button = browser.find_element(
+            *PROFILE_PAGE_LOCATORS["exit_button"])
+        browser.execute_script("arguments[0].click();", exit_button)
+
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((LOGIN_PAGE_LOCATORS["h2_enter"])))
         vis_btn = browser.find_element(*LOGIN_PAGE_LOCATORS["enter_button"])
