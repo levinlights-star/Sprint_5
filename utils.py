84097b0181f7d0002d4c3cd80f904fef2f504640
Svetlana <levinlights@gmail.com>
2026-04-09 15:42:33 +0700

    add register_user, update login_user

--- utils.py
+++ utils.py
@@ -1,29 +1,46 @@
 import random
-import time
+import logging
 
 from selenium.webdriver.common.by import By
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from pages.locators import LOGIN_PAGE_LOCATORS, MAIN_PAGE_LOCATORS
+from pages.locators import LOGIN_PAGE_LOCATORS, MAIN_PAGE_LOCATORS, REGISTER_PAGE_LOCATORS
 
 from data.test_data import VALID_USER
 
-from data.config import BASE_URL, LOGIN_URL
+from data.config import BASE_URL, REGISTRATION_URL
 
+logger = logging.getLogger(__name__)
 
-# Функция для авторизации пользователья на странице {BASE_URL}login
-def login_user(browser):
-    browser.current_url == LOGIN_URL
-    browser.find_element(
-        *LOGIN_PAGE_LOCATORS["email"]).send_keys(VALID_USER["email"])
-    browser.find_element(
-        *LOGIN_PAGE_LOCATORS["password"]).send_keys(VALID_USER["password"])
+# Функция авторизации пользователя на странице {BASE_URL}login
+def login_user(browser, email, password):
+    browser.find_element(*LOGIN_PAGE_LOCATORS["email"]).send_keys(email)
+    browser.find_element(*LOGIN_PAGE_LOCATORS["password"]).send_keys(password)
 
-    browser.find_element(*LOGIN_PAGE_LOCATORS["enter_button"]).click()
+    enter_button = browser.find_element(*LOGIN_PAGE_LOCATORS["enter_button"])
+    browser.execute_script("arguments[0].click();", enter_button)
 
     WebDriverWait(browser, 10).until(EC.url_to_be(BASE_URL))
+
+
+# Функция для регистрации пользователя
+def register_user(browser):
+    browser.get(REGISTRATION_URL)
+
+    browser.find_element(
+        *REGISTER_PAGE_LOCATORS["name"]).send_keys(VALID_USER["name"])
+    login = generate_email()
+    browser.find_element(*REGISTER_PAGE_LOCATORS["email"]).send_keys(login)
+    logger.info(f"Сгенерированный логин: {login}")
+    password = VALID_USER["password"]
+    browser.find_element(
+        *REGISTER_PAGE_LOCATORS["password"]).send_keys(password)
+    browser.find_element(*REGISTER_PAGE_LOCATORS["register_button"]).click()
+
+    WebDriverWait(browser, 5).until(EC.url_contains("/login"))
+    return login, password
 
 
 # Функция для генерации email
@@ -62,5 +79,6 @@ def get_ingredient(browser, list_index, item_index):
 
 # Функция клика по табам в конструкторе бургера
 def open_tab(browser, tab_locator):
-    tab = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(tab_locator))
+    tab = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located(tab_locator))
     browser.execute_script("arguments[0].click();", tab)
