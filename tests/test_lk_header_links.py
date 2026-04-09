fa820f9fdd50b8a4e40a24e9b3c916b7a5b2e872
Svetlana <levinlights@gmail.com>
2026-04-09 16:07:32 +0700

    update test_lk_header_links, add user registration

--- tests/test_lk_header_links.py
+++ tests/test_lk_header_links.py
@@ -7,7 +7,7 @@ from pages.locators import HEADERS_LOCATORS, MAIN_PAGE_LOCATORS, LOGIN_PAGE_LOCA
 
 from data.config import BASE_URL
 
-from utils import login_user
+from utils import login_user, register_user
 
 
 class TestLkHeaderLinks:
@@ -40,14 +40,17 @@ class TestLkHeaderLinks:
     # Авторизованный пользователь
     # Тест на переход по клику на «Конструктор» из личного кабинета, авторизованный пользователь
     def test_constructor_click_from_lk_user(self, browser):
+        email, password = register_user(browser)  # Регистрация пользователя
         browser.get(BASE_URL)
-        browser.find_element(*HEADERS_LOCATORS["account_link"]).click()
+        ac_link = browser.find_element(
+            *HEADERS_LOCATORS["account_link"])
+        browser.execute_script("arguments[0].click();", ac_link)
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((LOGIN_PAGE_LOCATORS["h2_enter"])))
-        login_user(browser)  # Авторизация пользователя
-
+        login_user(browser, email, password)      # Авторизация пользователя
         browser.find_element(*HEADERS_LOCATORS["account_link"]).click()
-        browser.find_element(*HEADERS_LOCATORS["сonstructor"]).click()
+        con = browser.find_element(*HEADERS_LOCATORS["сonstructor"])
+        browser.execute_script("arguments[0].click();", con)
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((MAIN_PAGE_LOCATORS["order_button"])))
         h1 = browser.find_element(*MAIN_PAGE_LOCATORS["h1_burgers"])
@@ -56,14 +59,18 @@ class TestLkHeaderLinks:
 
     # Тест на переход по клику на логотип Stellar Burgers из личного кабинета, авторизованный пользователь
     def test_logo_click_from_lk_user(self, browser):
+        email, password = register_user(browser)  # Регистрация пользователя
         browser.get(BASE_URL)
-        browser.find_element(*HEADERS_LOCATORS["account_link"]).click()
+        ac_link = browser.find_element(
+            *HEADERS_LOCATORS["account_link"])
+        browser.execute_script("arguments[0].click();", ac_link)
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((LOGIN_PAGE_LOCATORS["h2_enter"])))
-        login_user(browser)  # Авторизация пользователя
-
+        login_user(browser, email, password)      # Авторизация пользователя
         browser.find_element(*HEADERS_LOCATORS["account_link"]).click()
-        browser.find_element(*HEADERS_LOCATORS["logo"]).click()
+        logo = browser.find_element(
+            *HEADERS_LOCATORS["logo"])
+        browser.execute_script("arguments[0].click();", logo)
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((MAIN_PAGE_LOCATORS["order_button"])))
         h1 = browser.find_element(*MAIN_PAGE_LOCATORS["h1_burgers"])
